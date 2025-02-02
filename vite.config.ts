cc7b25713a7d6367342e208ad5c2f6000594cc76
LidaChk <lvglazko@gmail.com>
2025-02-03 00:39:42 +0300

    refactor:✨ some

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ command }: ConfigEnv): UserConfig => {
+  const base: string =
+    command === 'serve' ? '/' : '/react-rsschool-2025-deploy/class-components/';
+
+  return {
+    base,
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+        output: {
+          assetFileNames: (assetInfo: { name: string }): string => {
+            if (/\.(png|jpe?g|gif|svg|ico|webp)$/i.test(assetInfo.name)) {
+              return `assets/images/[name][extname]`;
+            }
+            return `assets/[name][extname]`;
+          },
+        },
+      },
     },
-  },
+    assetsInclude: ['/\.svg$/'],
+  } as UserConfig;
 });
