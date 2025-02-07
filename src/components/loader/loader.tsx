5c9076e5c86f956684b6a90e25bbbeab27a4a0d1
LidaChk <lvglazko@gmail.com>
2025-02-07 23:01:02 +0300

    refactor:✨ Loader to fc

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,17 +1,15 @@
-import { Component } from 'react';
+import React from 'react';
 import './loader.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="loader" aria-label="loading">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    );
-  }
-}
+const Loader: React.FC = () => {
+  return (
+    <div className="loader" aria-label="loading">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
 export default Loader;
