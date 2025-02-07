92fd18bdb32952172ae4440c99b4e6fabe48596f
LidaChk <lvglazko@gmail.com>
2025-02-07 23:36:17 +0300

    refactor:✨ App to fc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 
-import { Component } from 'react';
 import ErrorBoundary from './components/errorBoundary/errorBoundary';
 import Search from './components/search/search';
 import { LS_KEY_SEARCH_TERM } from './constants/constants';
@@ -8,70 +7,51 @@ import { searchResultItem } from './types/types';
 import { searchImages } from './api/nasaApi';
 import CardList from './components/cardList/cardList';
 import ErrorCatchWrapper from './components/errorCatchWrapper/errorCatchWrapper';
+import React, { useEffect, useState, useTransition } from 'react';
 
-type AppProps = object;
-interface AppState {
-  items: searchResultItem[];
-  isLoading: boolean;
-  error: string | null;
-  searchTerm: string;
-}
+const App: React.FC = () => {
+  const [items, setItems] = useState<searchResultItem[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem(LS_KEY_SEARCH_TERM) || ''
+  );
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoading: false,
-      error: null,
-      searchTerm: localStorage.getItem(LS_KEY_SEARCH_TERM) || '',
-    };
-  }
-  componentDidMount() {
-    this.fetchImages(this.state.searchTerm);
-  }
+  const [isPending, startTransition] = useTransition();
 
-  fetchImages = async (searchTerm: string) => {
+  const fetchImages = async (searchTerm: string) => {
     try {
-      this.setState({ isLoading: true, error: null });
+      setError(null);
 
       const response = await searchImages({ query: searchTerm });
 
-      this.setState({
-        items: response,
-        isLoading: false,
+      startTransition(() => {
+        setItems(response);
       });
     } catch (error) {
-      this.setState({
-        error: error instanceof Error ? error.message : 'An error occurred',
-        isLoading: false,
-      });
+      setError(error instanceof Error ? error.message : 'An error occurred');
     }
   };
 
-  onSearch = (newSearchTerm: string) => {
-    localStorage.setItem(LS_KEY_SEARCH_TERM, newSearchTerm);
+  useEffect(() => {
+    fetchImages(searchTerm);
+  }, [searchTerm]);
 
-    this.setState({ searchTerm: newSearchTerm }, () => {
-      this.fetchImages(newSearchTerm);
-    });
+  const onSearch = (newSearchTerm: string) => {
+    localStorage.setItem(LS_KEY_SEARCH_TERM, newSearchTerm);
+    setSearchTerm(newSearchTerm);
+    fetchImages(newSearchTerm);
   };
 
-  render() {
-    return (
-      <ErrorBoundary>
-        <h1>NASA Image search</h1>
-        <ErrorCatchWrapper error={this.state.error}>
-          <Search
-            onSearch={this.onSearch}
-            initialSearchTerm={this.state.searchTerm}
-          />
-          <CardList items={this.state.items} isLoading={this.state.isLoading} />
-          <div className="app"></div>
-        </ErrorCatchWrapper>
-      </ErrorBoundary>
-    );
-  }
-}
+  return (
+    <ErrorBoundary>
+      <h1>NASA Image search</h1>
+      <ErrorCatchWrapper error={error}>
+        <Search onSearch={onSearch} initialSearchTerm={searchTerm} />
+        <CardList items={items} isLoading={isPending} />
+        <div className="app"></div>
+      </ErrorCatchWrapper>
+    </ErrorBoundary>
+  );
+};
 
 export default App;
